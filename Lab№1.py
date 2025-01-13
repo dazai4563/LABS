03d360ee6e3c9dc480329acabece3a8240ed73ab
root <root@DESKTOP-I177CUH.localdomain>
2025-01-13 19:35:29 +0300

    попытка внесения лабораторной №1

---
+++ Lab№1.py
@@ -0,0 +1,24 @@
+# n = input()
+# if n.isdigit():
+#     n = int(n)
+#     for i in range(1, n + 1):
+#         print(i)
+# else:
+#     print('ошибка 00000000000000')
+
+
+
+# a = input(); b = input()
+# if a.isdigit() and b.isdigit():
+#     if int(a) < int(b):
+#         print(b, 'больше')
+#     elif int(a) > int(b):
+#         print(a, 'больше')
+#     else:
+#         print(a, '=', b)
+# else:
+#     print('ошибка 0000000000000')
+
+
+
+
