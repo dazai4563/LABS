b6801501eb036e680a8a837d3e528489e437d349
root <root@DESKTOP-I177CUH.localdomain>
2025-01-13 20:25:50 +0300

    внесение лабораторной №4

---
+++ mypackage/Lab2.py
@@ -0,0 +1,37 @@
+# from math import sqrt
+# a = int(input())
+# s = sqrt(a)
+# print(s)
+
+
+# from datetime import datetime
+# dat = datetime.utcnow()
+# print(dat)
+
+# def print_number_of_n(n):
+#     if n.isdigit():
+#         n = int(n)
+#         for i in range(1, n + 1):
+#             print(i)
+#     else:
+#         return ('ошибка 00000000000000')
+#
+#
+# class Person:
+#     def __init__(self, n):
+#         self.n = n
+#
+#     def scet(self):
+#         print(self.n)
+#
+#
+#
+#
+# import Lab2
+# Lab2.print_number_of_n('65')
+
+
+
+# from mypackage import  mymodule2
+# s = mymodule2.sum('34', '45')
+# print(s)
---
+++ mypackage/mymodule2.py
@@ -0,0 +1,3 @@
+def sum(a, b):
+    if a.isdigit() and b.isdigit():
+        return (int(a) + int(b))
