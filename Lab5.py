e65b91f0b7b25979770f425460ca162739131efe
root <root@DESKTOP-I177CUH.localdomain>
2025-01-13 21:04:30 +0300

    лабораторная №5

---
+++ Lab5.py
@@ -0,0 +1,33 @@
+# class Book:
+#     def __init__(self, title, author, year):
+#         self.title = title
+#         self.author = author
+#         self.year = year
+#
+#     def get_info(self):
+#         return('Название:',self.title, 'Автор:',self.author, 'год:',self.year)
+#
+# import Lab5
+#
+# book = Lab5.Book('fff', 'rrr', '444')
+# print(book.get_info())
+
+
+
+# class Circle:
+#     def __init__(self, radius, n_radius):
+#         self.radius = radius
+#         self.n_radius = n_radius
+#
+#
+#     def get_radius(self):
+#         return self.radius
+#
+#
+#     def new_radius(self):
+#         return self.n_radius
+#
+# import Lab5
+# radius = Lab5.Circle(45, 74)
+# print(radius.new_radius())
+# print(radius.get_radius())
